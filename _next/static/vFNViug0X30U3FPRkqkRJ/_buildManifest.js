97d7d07250bb3b7e1cce296859246b9af3fbf637
febrequency <febrequency@163.com>
2026-08-10 20:38:44 +0800

    Deploy PDF portfolio

---
+++ _next/static/vFNViug0X30U3FPRkqkRJ/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/zoe-s-portfolio//_next/:path+",
+        "destination": "/zoe-s-portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
